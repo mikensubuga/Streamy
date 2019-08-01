74cc6ac005a8770df6ea7ecc844075078f2a36ff
King-Mike <nsubugamike021@gmail.com>
2019-08-01 23:56:23 +0300

    creating forms

--- src/components/Streams/StreamCreate/StreamCreate.jsx
+++ src/components/Streams/StreamCreate/StreamCreate.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends Component {
+  render() {
+    return (
+      <div>
+        <div>StreamCreate</div>
+        <form>
+          <Field name="title" />
+          <Field name="description" />
+        </form>
+      </div>
+    );
+  }
+}
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
-
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
