9eef04208cd4539e73c07c06f379fe4f285254e4
King-Mike <nsubugamike021@gmail.com>
2019-08-02 20:25:33 +0300

    validation of form inputs

--- src/components/Streams/StreamCreate/StreamCreate.jsx
+++ src/components/Streams/StreamCreate/StreamCreate.jsx
@@ -37,6 +37,17 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: "streamCreate"
 })(StreamCreate);
