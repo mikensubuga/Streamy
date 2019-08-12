c1eee02639667f0cf9f4b3b6d504cdf2ad71b0c9
King-Mike <nsubugamike021@gmail.com>
2019-08-12 17:40:18 +0300

    rendering all streams

--- src/components/Streams/StreamList/StreamList.jsx
+++ src/components/Streams/StreamList/StreamList.jsx
@@ -5,8 +5,26 @@ class StreamList extends Component {
   componentDidMount() {
     this.props.onFetchStreams();
   }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            <a className="header">{stream.title}</a>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 const mapDispatchToProps = dispatch => {
@@ -16,7 +34,13 @@ const mapDispatchToProps = dispatch => {
     }
   };
 };
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams.stream)
+  };
+};
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(StreamList);
