b89d08c5f364a87cf20b304e231c70d0bd4b6df7
King-Mike <nsubugamike021@gmail.com>
2019-08-12 16:29:44 +0300

    edit stream red

--- src/store/reducers/streams.js
+++ src/store/reducers/streams.js
@@ -30,28 +30,51 @@ const reducer = (state = initialState, action) => {
         error: true
       };
 
-      case actionTypes.FETCH_STREAM_START:
-        return {
-          ...state,
-          loading: true,
-          error: false
-        };
-      case actionTypes.FETCH_STREAM_SUCCESS:
-        return {
-          ...state,
-          stream: {
-            ...state.stream,
-            [action.stream.id]: action.stream
-          },
-          loading: false,
-          error: false
-        };
-      case actionTypes.FETCH_STREAM_FAIL:
-        return {
-          ...state,
-          loading: false,
-          error: true
-        };
+    case actionTypes.FETCH_STREAM_START:
+      return {
+        ...state,
+        loading: true,
+        error: false
+      };
+    case actionTypes.FETCH_STREAM_SUCCESS:
+      return {
+        ...state,
+        stream: {
+          ...state.stream,
+          [action.stream.id]: action.stream
+        },
+        loading: false,
+        error: false
+      };
+    case actionTypes.FETCH_STREAM_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+
+    case actionTypes.EDIT_STREAM_START:
+      return {
+        ...state,
+        loading: true,
+        error: false
+      };
+    case actionTypes.EDIT_STREAM_SUCCESS:
+      return {
+        ...state,
+        stream: {
+          ...state.stream,
+          [action.stream.id]: action.response
+        },
+        loading: false,
+        error: false
+      };
+    case actionTypes.EDIT_STREAM_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
 
     default:
       return state;
