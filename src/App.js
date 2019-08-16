703ca515aed4707a96e6906b9d212ceb41e816ee
King-Mike <nsubugamike021@gmail.com>
2019-08-16 23:14:19 +0300

    wildcard navigation

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/edit/:id" exact component={StreamEdit} />
       </div>
     </div>
   );
